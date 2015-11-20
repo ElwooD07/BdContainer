23fcb4504b01058f24d26777f00e68f6c0bd8997
ElwooD07 <elwood0777@gmail.com>
2015-11-21 01:40:51 +0200

    Added moved and new headers

---
+++ trunk/proj/src/DbContainerLib/impl/TypesInternal.h
@@ -0,0 +1,23 @@
+#pragma once
+#include "Types.h"
+#include <memory>
+#include <mutex>
+
+namespace dbc
+{
+	/*
+	Usage:
+	class SomeClass
+	{
+		...
+		NONCOPYABLE(SomeClass)
+		...
+	}
+	It will work after any access specifier
+	*/
+	#define NONCOPYABLE_IMPL(TYPE) explicit TYPE(const TYPE&) = delete; TYPE& operator=(const TYPE&) = delete;
+	#define NONCOPYABLE(TYPE) NONCOPYABLE_IMPL(TYPE)
+
+	typedef std::lock_guard<std::mutex> MutexLock;
+	typedef std::shared_ptr<MutexLock> MutexLockGuard;
+}
