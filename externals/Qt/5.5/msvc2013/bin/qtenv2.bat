772958f61ca008f6d2106b052b9b546206ba4440
ElwooD07 <elwood0777@gmail.com>
2015-11-21 15:38:02 +0200

    Added Qt 5.5 to externals

---
+++ externals/Qt/5.5/msvc2013/bin/qtenv2.bat
@@ -0,0 +1,5 @@
+echo off
+echo Setting up environment for Qt usage...
+set PATH=C:\Qt\5.5\msvc2013\bin;%PATH%
+cd /D C:\Qt\5.5\msvc2013
+echo Remember to call vcvarsall.bat to complete environment setup!
